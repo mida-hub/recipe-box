8b20e9950ae75171f16ff23ccedad54171f9137b
mida-hub <rusuden0106@gmail.com>
2025-10-09 15:50:20 +0900

    get apiKey from env

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBEyPXsB73Q4haegSgT7d7nOxuv4WlkUmE",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "recipe-box-474414.firebaseapp.com",
   projectId: "recipe-box-474414",
   storageBucket: "recipe-box-474414.firebasestorage.app",
